6f734c491da09f43825a6f335762c9ce254f1096
Rajesh Boddu <61150374+razeshboddu@users.noreply.github.com>
2025-09-13 12:23:16 -0400

    Create udf_ConvertToDateTime.sql

---
+++ udf_ConvertToDateTime.sql
@@ -0,0 +1,18 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [dbo].[udf_ConvertToDateTime] (@Datetime BIGINT)
+	RETURNS DATETIME
+AS
+BEGIN
+    DECLARE @LocalTimeOffset BIGINT
+           ,@AdjustedLocalDatetime BIGINT;
+    SET @LocalTimeOffset = DATEDIFF(second,GETDATE(),GETUTCDATE())
+    SET @AdjustedLocalDatetime = @Datetime - @LocalTimeOffset
+    RETURN (SELECT DATEADD(second,@AdjustedLocalDatetime, CAST('1970-01-01 00:00:00' AS datetime)))
+END;
+
+GO
